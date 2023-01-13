7e785ab082d188ddf65fd3d6357ae6a8f001c281
xhifi <shifa.newversion@gmail.com>
2023-01-13 17:52:35 +0500

    Todo Mock

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,5 +1,30 @@
+import {useState} from 'react';
 import Main from '../../components/layouts/Main'
 
 export default function Landing () {
-    return <Main><h1>This is the Landing Page</h1></Main>
+    const [todo, setTodo] = useState("");
+    const [addTodos, setAllTodos] = useState([]);
+
+    const addTodo = (e) => {
+        e.preventDefault();
+        if(e.target.value){
+            setAllTodos([...addTodos, todo])
+        }else{
+            console.warn("No value")
+        }
+        
+        
+    }
+    
+    return (
+        <Main>
+            <form>
+                <label htmlFor="todoitem">Todo Name</label>
+                <input type="text" id="todoitem" onChange={e => setTodo(e.target.value)}/>
+                <button onClick={addTodo}>Add Todo</button>
+                <hr />
+                {addTodos.map((v, i) => <li key={i}>{v}</li>)}
+            </form>
+        </Main>
+    )
 }
